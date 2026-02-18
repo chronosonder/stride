d100bad45fd5fcbc8c9d951de56237baa1e8dafd
Cat <catalinbuhan@gmail.com>
2026-02-18 20:13:06 +0000

    Add database check in index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const knex = require('./src/db/knex');
+knex.raw('SELECT 1+1 AS result')
+    .then(() => {
+        console.log('Database connection successful!');
+    })
+    .catch((error) => {
+        console.error('Database connection failed:', error);
+        process.exit(1);
+    });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
