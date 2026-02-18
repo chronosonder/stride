dc2e742b03b67091cd1e52defccd4fa8017e15a1
Cat <catalinbuhan@gmail.com>
2026-02-18 20:13:53 +0000

    Create knexfile and knex config

---
+++ server/src/db/knex.js
@@ -0,0 +1,5 @@
+const knexfile = require("../../knexfile");
+const environment = process.env.NODE_ENV || 'development';
+const config = knexfile[environment];
+
+module.exports = require('knex')(config);
