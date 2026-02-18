5550386afc09c7240725edcd77a8595d4b8e05a1
Cat <catalinbuhan@gmail.com>
2026-02-18 23:22:49 +0000

    Create User model

---
+++ server/src/models/User.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(id, username, email, passwordHash, createdAt, updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // Safe object for API responses (excluded passwordHash)
+    toPublic() {
+        return {
+            id: this.id,
+            username: this.username,
+            email: this.email,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt
+        };
+    }
+
+    // Compare User object hashed password with provided password using
+    async veirfyPassword(password, bcrypt) {
+        return await bcrypt.compare(password, this.passwordHash);
+    }
+}
