100d548318d93cb316159d74842a08c76e277c54
Cat <catalinbuhan@gmail.com>
2026-04-07 19:03:26 +0100

    Update main.jsx
    Updated react-router import, added font and implemented custom theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+// Font imports
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import theme from './theme';
 import router from './routes/router';
 
 const queryClient = new QueryClient({
@@ -13,8 +21,6 @@ const queryClient = new QueryClient({
         }
     }
 });
-
-const theme = createTheme();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
